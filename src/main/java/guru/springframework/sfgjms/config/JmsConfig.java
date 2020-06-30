41490bbf8910c681fdad8d7f1f49a79af30ebcf6
helen <hhma@alumni.cpp.edu>
2020-06-29 22:26:28 -0400

    use spring's MessageConverter

--- src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgjms.config;
 
+//import org.apache.activemq.artemis.spi.core.protocol.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
